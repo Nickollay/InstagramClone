b0d2654994914fc4315ae14bfab4dba9912afb16
nickollay <nickollaysokolov@gmail.com>
2020-05-26 01:18:54 +0300

    add log in, sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # include Devise::Controllers::Helpers
+  # protect_from_forgery with: :exception
+
+  private
+
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+  #   else
+  #     redirect_to new_user_session_path
+  #   end
+  # end
 end
