c2ac06e2082b251820708c62033cda0a6976d1e2
nickollay <nickollaysokolov@gmail.com>
2020-05-28 23:27:04 +0300

    fix linx on aplication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   # include Devise::Controllers::Helpers
   # protect_from_forgery with: :exception
-  def after_sign_in_path_for(user)
-    user_posts_path(user)
-  end
+  #
+  # def after_sign_in_path_for(user)
+  #   user_posts_path(user)
+  # end
 
   private
 
