0cc28409d0ca5d07fcc9133fee2969f23f89b7b7
nickollay <nickollaysokolov@gmail.com>
2020-05-29 17:27:06 +0300

    add folow model & migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,18 @@ class User < ApplicationRecord
 
  devise :database_authenticatable, :registerable
 
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
+ has_many :posts, dependent: :destroy
+ has_many :comments, dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: EMAIL_FORMAT}
-  validates :name, presence: true
-  validates :bio, presence: true
+ has_many :followers, through: :follower_follows, source: :follower
+ has_many :follower_follows, foreign_key: :following_id, class_name: "Follow"
+
+ has_many :followings, through: :following_follows, source: :follower
+ has_many :following_follows, foreign_key: :follower_id, class_name: "Follow"
+
+ validates :email, presence: true, uniqueness: true
+ validates :email, format: { with: EMAIL_FORMAT}
+ validates :name, presence: true
+ validates :bio, presence: true
 
 end
